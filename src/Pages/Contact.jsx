e37c7b354c2701e9c796fc4124e0b75f51f20578
Enyim Chinonso <enyimchinonso23@gmail.com>
2025-05-18 05:35:53 +0100

    I built a simple React challenge using component rendering

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,5 +1,39 @@
 const Contact = () => {
-    return <h1>Contact Me</h1>;
-  };
+    return (
+      <>
+    <h1 style={{color:"red"}} align="center">Contact Me</h1>
+      
+    <form align="center">
+         <label>Enter your First name: <br />
+          <input type="text" />
+         </label> <br /> <br />
+  
+         <label>Enter your Last name: <br />
+          <input type="text" />
+         </label> <br /> <br />
+  
+         <label>Enter your Email Address: <br />
+          <input type="email" />
+         </label> <br /> <br />
+  
+         <label> Enter password: <br />
+          <input type="password" />
+         </label> <br /> <br />
+  
+          <label> Confirm Password: <br />
+          <input type="password" />
+         </label> <br /> <br />
+  
+         <button type='submit'>Submit</button>
+      </form>
+
+    </>
+  );
+  }
   
   export default Contact;
+
+
+
+
+  
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,53 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./Pages/Layout";
-import Home from "./Pages/Home";
-import Blogs from "./Pages/Blogs";
-import Contact from "./Pages/Contact";
-import NoPage from "./Pages/NoPage";
+import { createRoot } from "react-dom/client";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Layout from "./Pages/Layout";
+// import Home from "./Pages/Home";
+// import Blogs from "./Pages/Blogs";
+// import Contact from "./Pages/Contact";
+// import NoPage from "./Pages/NoPage";
 
 
-export default function App() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="blogs" element={<Blogs />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="*" element={<NoPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+// export default function App() {
+//     return (
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Layout />}>
+//             <Route index element={<Home />} />
+//             <Route path="blogs" element={<Blogs />} />
+//             <Route path="contact" element={<Contact />} />
+//             <Route path="*" element={<NoPage />} />
+//           </Route>
+//         </Routes>
+//       </BrowserRouter>
+//     );
+//   }
+
+function Challenge () {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/IMG_20240131_095209_547.jpg" width={200} alt="image" />
+      </header>
+      
+      <main>
+        <h1>My Career Path</h1>
+        <ol>
+          <li>My name is Enyim Chinonso.</li>
+          <li>I am a professional Software Engineer.</li>
+          <li>I write HTML, CSS, Bootstrap, javaScript and React.</li>
+          <li>I work with an Ameerican company that pays me $80,000 annually.</li>
+          <li>I am the future of Tech in Nigeria, A frica and worldwide.</li> 
+        </ol>
+      </main>
+
+      <footer>
+      <small>© 2024 BachelorTech development. All rights reserved.</small>
+
+      </footer>
+    </div>
+  );
+}
+
   
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(<App />);
+  const root = createRoot(document.getElementById('root'));
+  root.render(<Challenge />);
